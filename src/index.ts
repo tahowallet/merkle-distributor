ba1d4acfde464c7e5878b2e364dd56b8fa1d380f
Matt Luongo <mhluongo@gmail.com>
2021-10-28 14:11:58 -0400

    Package JS as well as smart contact artifacts
    Trying to figure out how to import Typescript source downstream is a
    pain. Packaging the entire build should simplify this.

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { default as BalanceTree } from "./balance-tree"
+
+export { default as MerkleTree } from "./merkle-tree"
+
+export { MerkleDistributorInfo, parseBalanceMap } from "./parse-balance-map"
